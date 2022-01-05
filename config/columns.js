d6ff441a2f022ef1a392e0657534772dfb97b06a
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-01-05 17:13:51 +0800

    Create columns.js

---
+++ config/columns.js
@@ -0,0 +1,109 @@
+[
+  {
+    title: `${moment().format('YYYY年MM月DD日')}工作量汇报`,
+    children: [
+      {
+        title: '姓名',
+        dataIndex: '0',
+      },
+      {
+        title: '本月业绩任务(万)',
+        dataIndex: '1',
+      },
+      {
+        title: '本月带看量任务(组)',
+        dataIndex: '2',
+      },
+      {
+        title: '本月重点新增(个)',
+        dataIndex: '3',
+      },
+      {
+        title: '本月微信通过目标(个)',
+        dataIndex: '4',
+      },
+      {
+        title: '带看',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '5',
+          },
+          {
+            title: '本周',
+            dataIndex: '6',
+          },
+        ],
+      },
+      {
+        title: '重点渠道新增',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '7',
+          },
+          {
+            title: '本周',
+            dataIndex: '8',
+          },
+        ],
+      },
+      {
+        title: '互动量',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '9',
+          },
+          {
+            title: '本周',
+            dataIndex: '10',
+          },
+        ],
+      },
+      {
+        title: '陪带量',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '11',
+          },
+          {
+            title: '本周',
+            dataIndex: '12',
+          },
+        ],
+      },
+      {
+        title: '微信通过量',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '13',
+          },
+          {
+            title: '本周',
+            dataIndex: '4',
+          },
+        ],
+      },
+      {
+        title: '线上带看量',
+        children: [
+          {
+            title: '今日',
+            dataIndex: '15',
+          },
+          {
+            title: '本周',
+            dataIndex: '16',
+          },
+        ],
+      },
+      {
+        title: '自己最喜欢励志的一句话',
+        dataIndex: '17',
+      },
+    ],
+  },
+]
