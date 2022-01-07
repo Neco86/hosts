1042e28dc4486afd87cb72726a4b0f8e80347ceb
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-01-07 17:52:32 +0800

    Add files via upload

---
+++ p__Clear.6dd5cf9c.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunktest=self.webpackChunktest||[]).push([[233],{15787:function(n,t,e){"use strict";e.r(t);var u=e(85893);t.default=function(){localStorage.clear();var a="https://neco86.github.io/templateReport/#/";return location.href=a,(0,u.jsx)(u.Fragment,{})}}}]);
